24379770b1fbbbfb5f938d6530ee9cf43458547a
Eduardo Demeneck Onghero <acrackintheice@gmail.com>
2017-01-24 22:52:29 -0200

    Adding things to git

---
+++ src/main/java/hello/dao/CandidatoDao.java
@@ -0,0 +1,25 @@
+package hello.dao;
+
+import java.util.List;
+
+import hello.model.Candidato;
+
+public interface CandidatoDao {
+
+	Candidato findById(int id);
+
+	List<Candidato> findByName(String name);
+	
+	Candidato findByCpf(String cpf);
+
+	void save(Candidato client);
+
+	void deleteById(int id);
+
+	void deleteByName(String name);
+
+	List<Candidato> findAll();
+
+	void deleteByCpf(String cpf);
+
+}
